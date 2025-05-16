9a05aa71e9705f5b2db1c068f67c43bbaee0f741
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-05-16 13:20:40 +0100

    Switch  to the latest 1.1.0 client

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from itertools import islice
 
 from bulk_chain.api import iter_content
 from bulk_chain.core.utils import dynamic_init
@@ -7,25 +8,23 @@ from tksheet import Sheet
 from utils import iter_test_jsonl_samples
 
 
-def callback(chunk, info, context, sheet):
+def callback(chunk, col, row, sheet):
 
-    reg = context["cols"]
-
-    # Register column number.
-    if info["param"] not in reg:
-        reg[info["param"]] = len(reg)
-
-    # Defining row index.
-    row = info["ind"] + context["row_ind"]
+    cols_to_ind = [
+        "r_aspect",
+        "r_opinion",
+        "r_polarity",
+        "r_label"
+    ]
 
     # Update cell data.
-    col = reg[info["param"]]
-    new_cell_data = sheet.get_cell_data(r=row, c=col) + chunk
-    sheet.set_cell_data(r=row, c=col, value=new_cell_data, redraw=True)
+    c =cols_to_ind.index(col)
+    new_cell_data = sheet.get_cell_data(r=row, c=c) + chunk
+    sheet.set_cell_data(r=row, c=c, value=new_cell_data, redraw=True)
 
     # Setup color.
     bg = "lightblue"
-    if info["param"] == "r_label":
+    if col == "r_label":
         content = new_cell_data.lower()
         if "positive" in content and "negative" not in content:
             bg = "lightgreen"
@@ -34,7 +33,7 @@ def callback(chunk, info, context, sheet):
         else:
             bg = "lightyellow"
 
-    sheet.highlight_cells(row, col, bg=bg, fg="black")  # Apply cell coloring
+    sheet.highlight_cells(row, c, bg=bg, fg="black")  # Apply cell coloring
 
     sheet.update()
 
@@ -42,19 +41,16 @@ def callback(chunk, info, context, sheet):
 def on_fill_button_click(sheet):
     """Event handler for the button to populate the sheet."""
 
-    batch_size = 1
-
-    ctx = {"row_ind": 0, "cols": {}}
-
-    data_it = iter_content(input_dicts_it=iter_test_jsonl_samples("data/sample.jsonl"),
+    data_it = iter_content(input_dicts_it=islice(iter_test_jsonl_samples("data/sample.jsonl"), sheet.total_rows()),
                            llm=llm,
                            return_batch=False,
-                           batch_size=1,
-                           callback_stream_func=lambda chunk, info: callback(chunk, info, ctx, sheet),
+                           batch_size=5,
+                           infer_mode="batch_stream_async",
+                           return_mode="chunk",
                            schema="data/thor_cot_schema.json")
 
-    for _ in data_it:
-        ctx["row_ind"] += batch_size
+    for ind, col, chunk in data_it:
+        callback(chunk=chunk, row=ind, col=col, sheet=sheet)
 
     sheet.redraw()
 
@@ -62,15 +58,15 @@ def on_fill_button_click(sheet):
 def main():
     root = tk.Tk()
     root.title("Sentiment Analysis Demo (meta/meta-llama-3-70b-instruct)")
-    root.geometry("850x800")
+    root.geometry("1320x800")
 
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
 
-    sheet = Sheet(root, total_rows=10, total_columns=8)
+    sheet = Sheet(root, total_rows=25, total_columns=4)
     sheet.grid(row=0, column=0, sticky="nsew")
     
-    sheet.set_column_widths([200] * 8)
+    sheet.set_column_widths([500, 300, 300, 150])
     sheet.set_row_heights([100] * 10)
 
     button = tk.Button(root, text="Fill Table", command=lambda: on_fill_button_click(sheet))
@@ -81,9 +77,8 @@ def main():
 
 
 if __name__ == "__main__":
-    llm = dynamic_init(class_dir=".",
-                       class_filepath="replicate_104.py",
-                       class_name="Replicate")(api_token="<NONE>",
+    llm = dynamic_init(class_filepath="replicate_104.py",
+                       class_name="Replicate")(api_token="YOUR-KEY-GOES-HERE",
                                                model_name="meta/meta-llama-3-70b-instruct",
                                                stream=True)
     main()
